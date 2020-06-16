a2102b3d7acb11df8353cc02a814cfeea06bddfe
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-16 18:13:34 +0530

    adding ejs and setting it up

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 //requiring express and defining the port number as a server runs on a port
 const express = require('express');
 const port = 8000;
+const path = require('path');
 
 /*we need to fire up express so we use app. and call express as function. This app has all the libraries
 needed to run a server*/
 const app = express();
 
 
+//setting up template engine to fetch records
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+//signifies that it is a middleware
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+
+var contactList = [
+    {
+        name : "Arpan",
+        phone : "11111"
+    },
+    {
+        name : "Anuj",
+        phone : "22222"
+    },
+    {
+        name : "Mahesh",
+        phone : "33333"
+    }
+]
+
+app.post('/create-contact',function(req,res){
+    contactList.push({
+        name:req.body.name,
+        phone:req.body.phone
+    });
+    return res.redirect('/');
+});
+
 app.get('/',function(req,res){
-    res.send("Running now");
+    return res.render('home',
+    {
+        title : "My Contact List",
+        contact_List : contactList
+    });
+});
+
+
+app.get('/practice',function(req,res){
+    return res.render('practice',{title : "My Contact List"});
 });
 
 

6876ed58b8a3c7fe84f4d45cf9dbb266cf330c9e
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-16 20:08:22 +0530

    deleted unseable code

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ const express = require('express');
 const port = 8000;
 const path = require('path');
 
-//const db = require('./config/mongoose');
-//const Contact = require('./model/contact');
-
 /*we need to fire up express so we use app. and call express as function. This app has all the libraries
 needed to run a server*/
 const app = express();
 
-
-//setting up template engine to fetch records
-//app.set('view engine','ejs');
-//app.set('views',path.join(__dirname,'views'));
 //signifies that it is a middleware
 
 app.use(express.urlencoded());
-
 app.use('/',require('./routes'))
 app.use(express.static('assets'));
 
@@ -25,56 +17,6 @@ app.use(express.static('assets'));
 app.set('view engine','ejs');
 app.set('views','./views');
 
-
-
-/*
-var contactList = [
-    {
-        name : "Arpan",
-        phone : "11111"
-    },
-    {
-        name : "Anuj",
-        phone : "22222"
-    },
-    {
-        name : "Mahesh",
-        phone : "33333"
-    }
-]
-
-app.post('/create-contact',function(req,res){
-    contactList.push({
-        name:req.body.name,
-        phone:req.body.phone
-    });
-    return res.redirect('/');
-    Contact.create({
-        name: req.body.name,
-        phone: req.body.phone
-    },function(err,newContact){
-        if(err){console.log('error in creatig a new contact');
-                return;}
-        console.log('***********',newContact);
-        return res.redirect('back');
-    });
-});
-
-app.get('/',function(req,res){
-    return res.render('home',
-    {
-        title : "My Contact List",
-        contact_List : contactList
-    });
-});
-
-
-app.get('/practice',function(req,res){
-    return res.render('practice',{title : "My Contact List"});
-});
-
-
-*/
 
 //run the server
 app.listen(port,function(err){
